ae5a4e57696b7c5a53e21b6dc9be7b6133ab2f4a
Brian Kuo <35882177+kuokai0429@users.noreply.github.com>
2023-04-24 12:47:52 +0800

    2023.0424.1247

--- common/dataset.py
+++ common/dataset.py
@@ -1,11 +1,9 @@
 # 2023.0420.0253 @Brian
 
-import torch
-
 class StrokeRecognitionDataset:
 
     def __init__(self, features, targets):
-        
+
         self.features = features
         self.targets = targets
         self.classes = {"其他": 0, "正手發球": 1, "反手發球": 2, "正手推球": 3, "反手推球": 4, "正手切球": 5, "反手切球":6}
