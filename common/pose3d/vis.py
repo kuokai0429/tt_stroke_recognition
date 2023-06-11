973faa009505f769c7b45b790b8b2ac49abf5791
Brian Kuo <35882177+kuokai0429@users.noreply.github.com>
2023-06-12 04:07:12 +0800

    2023.0612.0407

--- common/pose3d/vis.py
+++ common/pose3d/vis.py
@@ -154,7 +154,13 @@ def get_pose3D(video_path, output_dir):
     keypoints = np.load(output_dir + 'input_2D/keypoints.npz', allow_pickle=True)['reconstruction']
 
     cap = cv2.VideoCapture(video_path)
-    video_length = 3000 if int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) > 3000 else int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) 
+    
+    if int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) > 1000:
+        video_length = 1000
+        print("Long input frames detected! 1000 frames were evaluated.")
+        print("Use 'vis_longframes.py' for longer input frames...")  
+    else:
+        int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     ## 3D
     print('\nGenerating 3D pose...')
